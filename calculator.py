32cc30c007fedcffa39ccba992ac114fdc0365e8
ssideris <80916616+ssideris@users.noreply.github.com>
2021-03-18 20:13:59 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def main():
+    a=int(input('give a'))
+    b=int(input('give b'))
+
+    choice=input('add/sub/mal')
+    if choice =='add':
+        print(add(a,b))
+    elif choice=='sub':
+        print(sub(a,b))
+    else:
+        print(mul(a,b))
+
+main()
